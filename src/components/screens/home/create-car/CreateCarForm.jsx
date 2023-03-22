9b882b2ebb5e8ee64c1fe233bebc11fd48071c78
NovikovIlya <novikov.i.sergeevich@gmail.com>
2023-03-22 23:23:41 +0300

    ss

--- src/components/screens/home/create-car/CreateCarForm.jsx
+++ src/components/screens/home/create-car/CreateCarForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState,useEffect } from 'react'
 import styles from './CreateCarForm.module.css'
 import axios from 'axios';
-import {create} from 'zustand'
+
 
 const clearData = {
     price:'',
