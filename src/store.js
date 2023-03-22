e028ab19030efebcd7cbd6d2a8f6746ac29df4de
NovikovIlya <novikov.i.sergeevich@gmail.com>
2023-03-22 23:24:49 +0300

    6

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
-import { create } from 'zustand'
+// import { create } from 'zustand'
 
-export const useStore = create((set) => ({
-    count: 1,
-    inc: () => set((state) => ({ count: state.count + 1 })),
-  }))
+// export const useStore = create((set) => ({
+//     count: 1,
+//     inc: () => set((state) => ({ count: state.count + 1 })),
+//   }))
 
   
