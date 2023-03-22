ba5f66a4a8dbfc3edd121b9121944722fcc4d7a0
NovikovIlya <novikov.i.sergeevich@gmail.com>
2023-03-22 23:20:16 +0300

    11

--- src/components/screens/home/car-item/CarItem.jsx
+++ src/components/screens/home/car-item/CarItem.jsx
@@ -33,7 +33,7 @@ function CarItem({car}){
             {/* <Link className='linka' to='/'> ← Назад</Link> */}
             {/* <div className={styles.image} style={{backgroundImage:`url(${car.image})`}}></div> */}
             <div className={styles.info}>
-            <h1>{car.dataName}</h1>
+            <h1 className='highText'>{car.dataName}</h1>
             {/* {/person/ig.test(window.location.href)? <img className='images' src="https://img.freepik.com/premium-vector/occult-esoteric-tarot-card-symbol-baphomet-skull_8071-24482.jpg" alt="Avatar" /> : '' } */}
             {/* <h2>Узнайте</h2> */}
             {/* {/person/ig.test(window.location.href)? '' : <Link to={`person/${car.id}`} className='btn'>Узнать больше</Link> } */}
